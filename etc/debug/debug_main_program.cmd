e4a8532bc85adffa060b5a729fa17a728b5c267a
Ariel Montes <info@montesariel.com>
2024-06-01 15:32:56 +0200

    Add files via upload

---
+++ etc/debug/debug_main_program.cmd
@@ -0,0 +1,6 @@
+@ECHO OFF
+
+.\..\..\lib\python.exe ".\..\..\bin\main_program.pyw"
+
+ECHO: & ECHO PRESS A BUTTON TO EXIT...
+PAUSE > NUL
---
+++ etc/debug/debug_update_check.cmd
@@ -0,0 +1,6 @@
+@ECHO OFF
+
+.\..\..\lib\python.exe ".\..\..\bin\update_check.pyw" "v1.0.1"
+
+ECHO: & ECHO PRESS A BUTTON TO EXIT...
+PAUSE > NUL
